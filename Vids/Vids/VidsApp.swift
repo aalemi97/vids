0eb88a0b15f806de8294ac6129c220cab7988c2b
alireza <alireza.alemi76@icloud.com>
2024-06-16 16:42:22 -0600

    initialized ios workspace.

---
+++ Vids/Vids/VidsApp.swift
@@ -0,0 +1,17 @@
+//
+//  VidsApp.swift
+//  Vids
+//
+//  Created by AliReza on 2024-06-16.
+//
+
+import SwiftUI
+
+@main
+struct VidsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
